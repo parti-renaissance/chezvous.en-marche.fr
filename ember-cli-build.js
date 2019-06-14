89f975abbe36ad3b0ef0349f279254a764389fc0
Brian Whitton <me@brianwhitton.com>
2019-06-14 19:39:26 -0400

    webpack tweaking for ember auto import to successfully bring in algolia

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,4 +1,5 @@
 'use strict';
+const webpack = require('webpack');
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
@@ -12,8 +13,17 @@ module.exports = function(defaults) {
         'node_modules/include-media/dist',
       ],
     },
+    autoImport: {
+      webpack: {
+        plugins: [
+          new webpack.DefinePlugin({
+            'process.env.RESET_APP_DATA_TIMER': 60 * 2 * 1000,
+          })
+        ]
+      }
+    }
   });
-  
+
   app.import('node_modules/normalize.css/normalize.css');
   return app.toTree();
 };
