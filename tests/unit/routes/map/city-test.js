2d886429b445091b0d5da2e31c838b346d550429
Brian Whitton <me@brianwhitton.com>
2019-06-21 11:18:30 -0400

    update route unit test

--- tests/unit/routes/map/city-test.js
+++ tests/unit/routes/map/city-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | map', function(hooks) {
+module('Unit | Route | map/city', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:map');
+    let route = this.owner.lookup('route:map/city');
     assert.ok(route);
   });
 });
