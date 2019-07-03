1ce00a2c602130b90d8047dc8f229d3b5a3aa328
Rémi Gardien <remi@gardien.biz>
2019-07-03 17:18:17 +0200

    Add new summaries

--- app/components/summary/summary-templates.js
+++ app/components/summary/summary-templates.js
@@ -1,10 +1,42 @@
 export default {
-  creation_force_police_securite_quotidien: {
-    label: "Création d'une force de police de la sécurité du quotidien",
-    template: "Comme dans 61 autres villes de France, la police de sécurité du quotidien a été déployée dans certains quartiers de votre commune.",
+  suppression_taxe_habitation: {
+    label: "Suppression de la taxe d’habitation",
+    template: "Pour {{nombre_foyers}} foyers dans votre commune, la taxe d'habitation est en baisse de {{baisse_2018}} euros en 2018. Elle baissera de {{baisse_2019}} euros supplémentaires en 2019 et à nouveau en 2020. Au total, votre taxe d'habitation a donc baissé de {{baisse_total}} euros. Pour savoir si vous êtes éligibles à la baisse rendez vous <a href=\"#\">ici</a>.",
   },
-  total_nouvelles_maisons_service_au_public_departement: {
-    label: "Création d'une maison de service au public",
-    template: "{{nouvelles_maisons}} maison de service au public a/ont été ouvertes dans votre département."
+  couverture_fibre: {
+    label: "Couverture en fibre de tout le territoire",
+    template: "Dans votre commune, il y a désormais {{nombre_locaux_raccordes_ville}} locaux raccordés au Très Haut Débit, soit {{hausse_depuis_2017_ville}} de plus qu'en mai 2017. Dans votre département, il y a désormais {{nombre_locaux_raccordes_departement}} locaux raccordés au Très Haut Débit, soit {{hausse_depuis_2017_departement}} de plus qu'en mai 2017."
+  },
+  maison_service_accueil_public: {
+    label: "Généralisation des maisons de service et d'accueil du public",
+    template: "Une Maison de service et d’accueil du public a ouverte dans votre commune."
+  },
+  creation_entreprises: {
+    label: "Créations nettes d’entreprises dans la commune",
+    template: "Depuis mai 2017, il y a {{entreprises}} entreprises de plus dans votre commune."
+  },
+  baisse_nombre_chomeurs: {
+    label: "Baisse du nombre de chômeurs",
+    template: "\"Depuis mai 2017, il y a {{baisse_ville}} chômeurs en moins dans votre commune. Depuis mai 2017, il y a {{baisse_departement}} chômeurs en moins dans votre département."
+  },
+  pass_culture: {
+    label: "Mise en Place du Pass Culture",
+    template: "Dans votre commune, tous les jeunes de 18 ans peuvent expérimenter le PASS culture : c’est 500€ pour s’offrir des activités culturelles et artistiques."
+  },
+  emplois_francs: {
+    label: "Emplois francs",
+    template: "Un quartier de votre commune est éligible au dispositif des emplois francs."
+  },
+  creation_police_securite_quotidien: {
+    label: "Création d'une force de police de sécurité du quotidien",
+    template: "La police de sécurité du quotidien a été déployée dans votre ville."
+  },
+  cheque_energie: {
+    label: "Chèque énergie",
+    template: "Dans votre département, {{nombre_beneficiaires}} personnes bénéficient d’un chèque énergie, pour un montant moyen de 200 euros."
+  },
+  conversion_surface_agricole_bio: {
+    label: "Conversion de la surface agricole en Bio",
+    template: "Dans votre département, {{hectares_bio}} hectares sont désormais cultivés en agriculture biologique, ou sont en cours de conversion. C’est {{progression}} de plus qu’en 2017."
   },
 };
