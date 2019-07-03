a2d62750d51d99c228414861ea9ee39058437908
Brian Whitton <me@brianwhitton.com>
2019-07-02 23:11:54 -0400

    attrs are now camelCased from algolia

--- mirage/serializers/city-hit.js
+++ mirage/serializers/city-hit.js
@@ -1,5 +1,7 @@
 import { Serializer } from 'ember-cli-mirage';
+import { camelize } from '@ember/string';
 
 export default Serializer.extend({
   keyForModel: () => 'hits',
+  keyForAttribute: attr => camelize(attr),
 });
