f697ba8ffca4b764b6cb7043cf21bbe56b1b9dfc
Brian Whitton <me@brianwhitton.com>
2019-07-03 10:20:02 -0400

    ensure multiple variables are replaced

--- tests/integration/components/summary/component-test.js
+++ tests/integration/components/summary/component-test.js
@@ -6,26 +6,29 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | summary', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it handles templates with multiple variables', async function(assert) {
     const TEMPLATES = {
       foo: {
         label: 'Foo Label',
-        template: 'There were {{number}} new foos',
+        template: 'There were {{foo}} new foos and {{bar}} new bars',
       }
     };
     const SUMMARY = {
       type: 'foo',
       data: {
-        number: 100,
+        foo: 100,
+        bar: 500,
       }
     };
+
+    const EXPECTED = 'There were 100 new foos and 500 new bars'
 
     this.setProperties({TEMPLATES, SUMMARY});
 
     await render(hbs`<Summary @TEMPLATES={{TEMPLATES}} @summary={{SUMMARY}}/>`);
 
     assert.dom('[data-test-summary-label]').hasText(TEMPLATES.foo.label);
-    assert.dom('[data-test-summary-dek]').hasText(TEMPLATES.foo.template.replace('{{number}}', 100));
+    assert.dom('[data-test-summary-dek]').hasText(EXPECTED);
   });
 
   test('it handles data without a corresponding template', async function(assert) {
