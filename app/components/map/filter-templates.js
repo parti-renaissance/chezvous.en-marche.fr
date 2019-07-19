f05935c18957014c7b08b33947a94366eb95deb4
Rémi Gardien <remi@gardien.biz>
2019-07-19 11:15:11 +0200

    Add extra markers & measures

--- app/components/map/filter-templates.js
+++ app/components/map/filter-templates.js
@@ -3,4 +3,12 @@ export default {
     label: "Dédoublement des classes",
     color: 'azure',
   },
+  mission_bern: {
+    label: "Mission Bern",
+    color: 'watermelon',
+  },
+  maison_service_accueil_public: {
+    label: "Nouvelle maison de service et d'accueil au public",
+    color: 'marigold',
+  },
 };
